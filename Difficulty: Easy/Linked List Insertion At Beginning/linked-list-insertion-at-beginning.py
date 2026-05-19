e0b291e103779b0bdb8ced15eb08acdf9ae11bc9
soham08-sm <soham08.edu@gmail.com>
2026-05-19 12:10:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.py
@@ -0,0 +1,15 @@
+"""
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+"""
+
+class Solution:
+    def insertAtFront(self, head, x):
+        #code here
+        first = Node(x)
+        first.next = head
+        head  = first
+        return head
