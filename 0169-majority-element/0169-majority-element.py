5256a05acc49c5aa17c478b6c8a9b6410129335a
soham08-sm <soham08.edu@gmail.com>
2026-05-19 10:42:23 +0530

    Time: 15 ms (19.34%), Space: 21.3 MB (20.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+            if freq[num] > len(nums) // 2:
+                return num
+
+        return -1
