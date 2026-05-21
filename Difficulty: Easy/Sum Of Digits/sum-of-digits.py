e0bdbf83ea1559af721c62e061542682742469d1
soham08-sm <soham08.edu@gmail.com>
2026-05-21 15:51:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfDigits(self, n):
+        # code here
+        total = 0
+        
+        while n > 0:
+            digit = n % 10
+            total += digit
+            n = n // 10
+            
+        return total
